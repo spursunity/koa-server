569edabfbd96cf7edddefea6dc047c8e3dbeb5c7
spursunity <spursunity@mail.ru>
2019-08-09 17:28:47 +0300

    send static files by mount+serve

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const Koa = require('koa');
+const serve = require('koa-static');
+const mount = require('koa-mount');
 
+const page = new Koa();
 const app = new Koa();
 const port = 3000;
 
-app.use(async (ctx) => {
-  ctx.body = 'Hello world';
-});
+page.use(serve('./public'));
+
+app.use(mount('/', page));
 
 app.listen(port, console.log('server started on port ' + port));
