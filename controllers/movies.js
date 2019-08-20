e6a95d8342a7ff31352393713cae383842010d44
spursunity <spursunity@mail.ru>
2019-08-20 19:59:14 +0300

    feature: add handler for movie year

---
+++ controllers/movies.js
@@ -0,0 +1,36 @@
+const { Movie } = require('../models');
+
+/**
+ * query params are
+ * year: Number
+ * time: Number [0, 1]
+ */
+async function findMovieByYear(ctx) {
+  try {
+    const { year = "2019", time = "0" } = ctx.query;
+    let response = [];
+    const movieYear = parseInt(year, 10);
+    const movieTime = parseInt(time, 10);
+
+    switch (movieTime) {
+      case 0:
+        response = await Movie.find({ year: { $lte: movieYear } }) || [];
+        break;
+      case 1:
+        response = await Movie.find({ year: { $gte: movieYear } }) || [];
+        break;
+      default:
+        throw new Error('bad query params');
+    }
+    ctx.status = 200;
+    ctx.body = response;
+  } catch (e) {
+    console.log(e.message);
+    ctx.status = 500;
+    ctx.body = e.message;
+  }
+};
+
+module.exports = {
+  findMovieByYear,
+};
