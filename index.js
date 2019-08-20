5e20aa63f9e8d11f0c97b2b719fccb55804b17b1
spursunity <spursunity@mail.ru>
2019-08-20 19:58:01 +0300

    fix: add connection with DB

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const Koa = require('koa');
 const serve = require('koa-static');
 const mount = require('koa-mount');
 
+const mongooseConnect = require('./config');
+const { findMovieByYear } = require('./controllers/movies');
+
+mongooseConnect();
 const page = new Koa();
 const app = new Koa();
 const port = 3000;
 
 page.use(serve('./public'));
 
-async function hello(ctx) {
-  ctx.body = { name: 'rock' };
-};
-
 app.use(mount('/', page));
-app.use(mount('/hello', hello));
+app.use(mount('/api', findMovieByYear));
 
 app.listen(port, console.log('server started on port ' + port));
